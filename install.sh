1b006a8a9f1d73e35c6bf86524c72027c72e7ff1
noir <130840857+yomi2998@users.noreply.github.com>
2026-02-23 15:58:22 +0800

    add blueman

--- install.sh
+++ install.sh
@@ -135,7 +135,7 @@ main() {
                 chromium firefox-esr
                 fcitx5 fcitx5-chinese-addons fonts-noto* fonts-font-awesome
                 build-essential cmake make gdb lldb clang clang-format llvm nlohmann-json3-dev
-                nano git curl wget ntfs-3g aria2 flatpak autotiling network-manager network-manager-applet seahorse
+                nano git curl wget ntfs-3g aria2 flatpak autotiling network-manager network-manager-applet seahorse blueman
         "
         install_packages "$GENERAL_PACKAGES"
         install_flatpak_apps
